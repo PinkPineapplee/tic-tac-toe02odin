bc7acab1bd1f9b877dee22757a58737411e00c71
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-03-27 22:34:47 +0100

    make playerConstructor

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const startBtn = document.querySelector("button");
 
 
 
-game ={
-  board : [null,null,null,null,null,null,null,null,null]
+game = {
+  board : [null,null,null,
+           null,null,null,
+           null,null,null],
 
-}
+  start : false,
+  turn: playerOne.marker
+};
+
+ function Player(name,marker){
+   this.name=name;
+   this.marker=marker;
+   this.score = 0;
+ }
