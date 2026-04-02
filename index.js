1072fbfb801bc4d6f74f221aecb9104cc28a40c1
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2026-04-02 09:27:08 +0100

    Feat: add new method printboard

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-
+const prompt = require("prompt-sync")();
 const boxes = document.querySelectorAll(".box");
 const startBtn = document.querySelector("button");
 
 
 
-game = {
+const game = {
   board : [null,null,null,
            null,null,null,
            null,null,null],
 
-  start : false,
-  turn: playerOne.marker
+  active : false,
+  currentPlayer: playerOne.marker,
+
+  printBoard: function(){
+                console.log(`
+                    ${game.board[0]} | ${game.board[1]} | ${game.board[2]}
+                    ---------
+                    ${game.board[3]} | ${game.board[4]} | ${game.board[5]}
+                    ---------
+                    ${game.board[6]} | ${game.board[7]} | ${game.board[8]}
+                  `)
+  }
+  
 };
 
  function Player(id,marker){
@@ -22,6 +33,12 @@ game = {
  const playerOne = new Player(1, "X");
  const playerTwo = new Player(2, "O");
 
+
+ //make game controlflow
+const controlFlow = {
+  
+}
 function playGame(){
   game.start = true;
 }
+
